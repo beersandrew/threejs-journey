c783ef5e4c1fa0e9ba4310665fb0899e12c55437
andy <acbeers1@gmail.com>
2023-05-21 18:17:38 -0400

    Finished debug lesson

---
+++ 44-debug-a-r3f-application/src/Experience.jsx
@@ -0,0 +1,47 @@
+import { OrbitControls } from '@react-three/drei'
+import { button, useControls} from 'leva'
+import { Perf } from 'r3f-perf'
+
+export default function Experience()
+{
+
+    const {position, color} = useControls( {
+        position: { 
+            value: { x: -2, y: 0}, 
+            joystick: 'invertY',
+            step: 0.01
+        },
+        color: '#ff0000',
+        clickMe: button(() => { console.log('ok')}),
+        choice: {
+            options: [ 'a', 'b', 'c']
+        }
+    });
+
+
+
+    return <>
+
+        <Perf position="top-left"/>
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <mesh position={ [position.x, position.y, 0] }>
+            <sphereGeometry />
+            <meshStandardMaterial color={color} />
+        </mesh>
+
+        <mesh position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
