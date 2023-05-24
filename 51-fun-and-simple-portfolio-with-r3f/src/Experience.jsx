e135c64e595f3c2e87a42538a87f3270fd2af4b2
andy <acbeers1@gmail.com>
2023-05-24 17:41:59 -0400

    finished fun and simple portfolio

---
+++ 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -0,0 +1,66 @@
+import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+
+export default function Experience()
+{
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    console.log(computer);
+    
+    return <>
+
+
+        <Environment preset="city"/>
+        <color args={['#241a1a']} attach='background'/>
+
+        
+        <PresentationControls global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={ { mass: 2, tension: 400}}
+        snap={ { mass: 4, tension: 400}}
+        >
+            <Float
+                rotationIntensity={0.4}>
+                <primitive object={ computer.scene }
+                    position-y={-1.2}
+                >
+                    <rectAreaLight 
+                        width={ 2.5 }
+                        height={ 1.65 }
+                        intensity={ 65 }
+                        color={ '#000000' }
+                        rotation={ [ - 0.1, Math.PI, 0 ] }
+                        position={ [ 0, 0.55, - 1.15 ] }
+
+                    />
+                    <Html transform
+                    wrapperClass="htmlScreen"
+                    distanceFactor={1.17}
+                    position={[0, 1.56, -1.4]}
+                    rotation-x={-0.256}> 
+                        <iframe src="https://andrew.beer/s" />
+                    </Html>
+                </primitive>
+
+                <Text
+                    font="./bangers-v20-latin-regular.woff"
+                    fontSize={0.75}
+                    position={[2, 0.75, 0.75]}
+                    rotation-y={-1.25}
+                    maxWidth={2}
+                    textAlign='center'
+                > Andy Beers</Text>
+            </Float>
+        </PresentationControls>
+        
+        <ContactShadows
+            position-y={-1.4}
+            opacity={0.4}
+            scale={5}
+            blur={2.4}
+        >
+
+
+        </ContactShadows>
+    </>
+}
