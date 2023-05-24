ce24c6b8b238032b89591293d3587d07900bdfe6
andy <acbeers1@gmail.com>
2023-05-24 17:41:32 -0400

    Finished mouse events

---
+++ 49-mouse-events-with-r3f/src/Experience.jsx
@@ -0,0 +1,43 @@
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+
+export default function Experience()
+{
+    const cube = useRef()
+    
+    useFrame((state, delta) =>
+    {
+        cube.current.rotation.y += delta * 0.2
+    })
+
+    const eventHandler = (event) => {
+        console.log('sup', event)
+
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)))`)
+    }
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh onClick={eventHandler} ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
