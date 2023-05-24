2f6d45a9fc43d3b7bd5b0a87021acd8b61544cb0
andy <acbeers1@gmail.com>
2023-05-24 17:42:25 -0400

    finished create a game

---
+++ 53-create-a-game-with-r3f/src/Experience.jsx
@@ -0,0 +1,27 @@
+import Lights from './Lights.jsx'
+import Level from './Level.jsx'
+import { Physics, Debug } from '@react-three/rapier'
+import Player from './Player.jsx'
+import useGame from './stores/useGame.jsx'
+import Effects from './Effects.jsx'
+
+
+export default function Experience()
+{
+    const blocksCount = useGame((state) => {return state.blocksCount})
+    const blocksSeed = useGame(state => state.blocksSeed)
+    return <>
+
+    <color args={ [ '#252731' ] } attach="background" />
+
+        <Physics>
+            {/* <Debug /> */}
+            <Lights />
+            <Level count={ blocksCount} seed={blocksSeed}/>
+            <Player />
+        </Physics>
+
+        <Effects />
+
+    </>
+}
